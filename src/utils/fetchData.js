8820d83469a0d9891d6112f06ef7f4cb775e6d45
Aniruddha Pandurang Falke <aniruddhafalke685@gmail.com>
2024-07-14 12:50:14 +0530

    Files and Foulder Added

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': '10f9c54d29msh6404ac548d36e51p19ce75jsn6aef08ab48d4',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
